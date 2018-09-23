c1ff4bf926df4fc9f6ed10335737bad5fcafdcdc
easherma <ericandrewsherman@gmail.com>
2018-09-23 12:26:22 -0500

    port Show.js to article and home/ArticlesList to articles/index.js

---
+++ pages/articles/article.js
@@ -0,0 +1,117 @@
+import React from "react"
+import { connect } from "react-redux"
+import Markdown from "markdown-to-jsx"
+import styled from "styled-components"
+import theme from "theme"
+import { getArticle } from "ducks/articles"
+import Loader from "components/Loader"
+import { Grid, Column } from "components/Grid"
+import { BlockLink } from "components/TouchTarget"
+
+const StyledArticle = styled(Grid)`
+  padding: 10rem 1rem;
+  min-height: 100vh;
+  .article__hero-image {
+    max-width: 100%;
+  }
+  .article__body {
+    margin-bottom: 2rem;
+    h2,
+    h3,
+    h4 {
+      margin-top: 4rem;
+    }
+    p {
+      margin-top: 2rem;
+      line-height: 2;
+    }
+    img {
+      max-width: 100%;
+    }
+  }
+  .contributor {
+    display: flex;
+    align-items: center;
+  }
+  .contributor__info {
+    flex: 1;
+    line-height: 2;
+  }
+  .contributor__image {
+    width: 5rem;
+    height: 5rem;
+    border-radius: 2.5rem;
+    margin-right: 1rem;
+    box-shadow: 0 12px 12px -6px rgba(0, 0, 0, 0.12);
+  }
+  @media (min-width: ${theme.breakPoints.sm}) {
+    padding: 10rem 0;
+  }
+`
+
+class Article extends React.Component {
+  state = { loaded: false }
+  componentDidMount = () =>
+    this.props
+      .getArticle(this.props.match.params.article_id)
+      .then(() => this.setState({ loaded: true }))
+  render = () => {
+    const { loaded } = this.state
+    if (!loaded)
+      return (
+        <StyledArticle>
+          <Column>
+            <Loader />
+          </Column>
+        </StyledArticle>
+      )
+    const {
+      article: {
+        fields: { contributor, photo, title, body }
+      }
+    } = this.props
+    return (
+      <StyledArticle container>
+        <Column smOffset={1} sm={11}>
+          <BlockLink color="black" to="/">
+            Back
+          </BlockLink>
+        </Column>
+        <Column smOffset={1} sm={8}>
+          <h1>{title}</h1>
+          {photo && (
+            <img
+              className="article__hero-image"
+              src={photo.fields.file.url}
+              alt="hero image"
+            />
+          )}
+          <Markdown className="article__body">{body}</Markdown>
+          {contributor && (
+            <React.Fragment>
+              <p>About the author: </p>
+              <div className="contributor">
+                <img
+                  className="contributor__image"
+                  src={contributor.fields.photo.fields.file.url}
+                  alt={`${contributor.fields.name}'s photo`}
+                />
+                <div className="contributor__info">
+                  <p className="name">{contributor.fields.name}</p>
+                  <p className="bio">{contributor.fields.bio}</p>
+                </div>
+              </div>
+            </React.Fragment>
+          )}
+        </Column>
+      </StyledArticle>
+    )
+  }
+}
+
+export default connect(
+  ({ articles: { items } }, ownProps) => ({
+    article: items[ownProps.match.params.article_id]
+  }),
+  { getArticle }
+)(Article)
---
+++ pages/articles/index.js
@@ -0,0 +1,114 @@
+import React from "react"
+
+import styled from "styled-components"
+import theme from "theme"
+import { Grid, Column } from "components/Grid"
+import Loader from "components/Loader"
+import { Link } from "next/link"
+
+
+const ArticleListWrapper = styled(Grid)`
+  margin-top: 10rem;
+  margin-bottom: 10rem;
+  .small-article-grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2rem;
+  }
+  .small-article {
+    grid-column: span 2;
+  }
+`
+
+const StyledArticle = styled.article`
+  position: relative;
+  .article__image {
+    padding-bottom: 62.5%;
+    background-color: ${theme.colors.primary["500"]};
+    background-image: url(${props => props.photo});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+  }
+  .article__title {
+    margin-top: 2rem;
+  }
+  .article__contributor {
+    margin-top: 1rem;
+  }
+  .article__link {
+    margin-top: 1rem;
+  }
+  a {
+    color: ${theme.colors.grey["900"]};
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`
+
+const Article = ({
+  article: {
+    fields: { photo, title, contributor },
+    sys
+  },
+  className
+}) => {
+  return (
+    <StyledArticle
+      className={className}
+      photo={photo ? photo.fields.file.url : "no-photo"}
+    >
+      <Link to={`/articles/${sys.id}`}>
+        <div className="article__image" />
+
+      <h4 className="article__title">{title}</h4>
+      {contributor && (
+        <p className="article__contributor">{contributor.fields.name}</p>
+      )}      </Link>
+    </StyledArticle>
+  )
+}
+
+class ArticleList extends React.Component {
+  componentDidMount = () => this.props.getArticles()
+
+  render = () => {
+    const { articles, loaded } = this.props
+    const articlesArray = Object.values(articles)
+    // first article
+    const head = articlesArray[0]
+    // copy because slice mutates articlesArray.
+    const tail = [...articlesArray.slice(1)]
+    if (!loaded) return <Loader />
+    return (
+      <ArticleListWrapper container>
+        <Column smOffset={1} sm={10}>
+          <h4>Featured Articles</h4>
+        </Column>
+        <Column md={5} smOffset={1}>
+          <Article article={head} />
+        </Column>
+        <Column md={5} className="small-article-grid">
+          {tail.map(article => (
+            <Article
+              key={article.sys.id}
+              article={article}
+              className="small-article"
+            />
+          ))}
+        </Column>
+      </ArticleListWrapper>
+    )
+  }
+}
+
+export default connect(
+  ({ articles: { items, loaded } }) => ({
+    articles: items,
+    loaded
+  }),
+  { getArticles }
+)(ArticleList)
